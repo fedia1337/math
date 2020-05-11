344b4405b1cb7fad2f4f42497ac1a28ef79e53e7
Fedia Zhukov <fedia77792@gmail.com>
2020-05-11 16:14:43 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+console.log("hello andrew")
+
+// --- first (green) ---------------------------
+const firstW = 4
+const firstH = 3
+const firstS = firstW * firstH
+
+const first = document.getElementById ("first")
+first.style.width = `${firstW}cm`
+first.style.height = `${firstH}cm`
+
+console.log(firstS)
+
+
+// --- second (orange) -----------------------
+
+/*
+
+const first = 3
+const firstH = 3
+const firstS = firstW * firstH
+
+const first = document.getElementById ("first")
+first.style.width = `${firstW}cm`
+first.style.height = `${firstH}cm`
+
+console.log(firstS)
+
+ */
