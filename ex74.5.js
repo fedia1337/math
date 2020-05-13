c73e90217f64b29d5024da53c145176d592cf575
Fedia Zhukov <fedia77792@gmail.com>
2020-05-13 13:08:17 +0300

    заготовка для нового упражнения(86.8)

---
+++ ex74.5.js
@@ -0,0 +1,53 @@
+console.log("hello andrew")
+
+function coolj(){
+    return "cool"
+}
+
+function square(width, height) {
+    return width * height
+}
+
+
+// --- main -------------------
+export function main() {
+
+    // --- first (green) ---------------------------
+    const firstW = 4
+    const firstH = 3
+    const firstS = square(firstW, firstH)
+
+    const first = document.getElementById("first")
+    first.style.width = `${firstW}cm`
+    first.style.height = `${firstH}cm`
+    first.innerText = "S = " + firstS
+
+    console.log(firstS)
+
+
+    // --- second (orange) -----------------------
+
+    const secondW = 3
+    const secondH = 3
+    const secondS = square(secondH, secondW)
+
+    const second = document.getElementById("second")
+    second.style.width = `${secondW}cm`
+    second.style.height = `${secondH}cm`
+    second.innerText = "S = " + secondS
+    console.log(secondS)
+
+
+    // --- fedia (pink) -----------------------
+    const fediaW = 4
+    const fediaH = 2
+    const fediaS = square(fediaW, fediaH)
+
+    const fedia = document.getElementById("fedia")
+    fedia.style.width = `${fediaW}cm`
+    fedia.style.height = `${fediaH}cm`
+    fedia.innerText = "S = " + fediaS
+
+    console.log(fediaS)
+
+}
