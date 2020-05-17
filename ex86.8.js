6b195087ae055ecb62fe89c115a2f136dd8320a8
Fedia Zhukov <fedia77792@gmail.com>
2020-05-17 11:45:18 +0300

    new exercises..

--- ex86.8.js
+++ ex86.8.js
@@ -1,3 +1,36 @@
+let numOfLichilnikiv = 21
+let numOfDniv = 7
+let vv =  numOfLichilnikiv /  numOfDniv
+
+let результат = vv * 14
+
+
+
+
+
+let s = 36
+let t1 = 7
+let t2 = 5
+
+let v = s / (t1 + t2)
+let n1 = v * t1
+let n2 = v * t2
+
+let resuntString = 
+"Відповідь:1 електрик установив  " + n1 +" лічильників " + "2 електрик установив " + n2 + " лічильників"
+
+
+
+
+
+
+
 export function main(){
-    return "hello"
+    //return "електрик установил " + результат + "лічильники"
+    return  resuntString
 }
+
+
+
+
+
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-import {main} from './ex86.8'
-
-let result = main()
+//import {main} from './ex86.8'
+//import {ex1, ex2} from "./p.21"
+//import {ex3} from "./p.22"
+import {ex4} from "./p.12"
+let result = ex4 ()
 console.log(result)
